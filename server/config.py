a9dd1cc2963634d356235473e76066800113e64a
kelvin <kelvinmutugi336@gmil.com>
2024-07-09 07:43:14 +0300

    rectifies error

--- server/config.py
+++ server/config.py
@@ -1,6 +1,6 @@
-import os
+# import os
 
-# Flask app configuration
-SECRET_KEY = 'your_secret_key_here'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'  # SQLite for simplicity
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# # Flask app configuration
+# SECRET_KEY = 'your_secret_key_here'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
